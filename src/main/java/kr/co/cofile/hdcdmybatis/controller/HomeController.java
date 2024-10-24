13b991e6db2bc7de18d06292945f0773e30d339b
Austine <chucky891213@naver.com>
2024-10-24 14:14:17 +0900

    MR: 인메모리인증(SecurityFilterChain, HttpSecurity, UserDetailsService 이용) 작업

--- src/main/java/kr/co/cofile/hdcdmybatis/controller/HomeController.java
+++ src/main/java/kr/co/cofile/hdcdmybatis/controller/HomeController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
 	@GetMapping({"/", "/home"})
 	public String home() {
 		return "home";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
